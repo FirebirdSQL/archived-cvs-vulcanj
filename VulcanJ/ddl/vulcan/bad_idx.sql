18b2640f6bad6f5cbe7c29dd89f3a349c9946454
Bill Oliver <verbguy@users.sourceforge.net>
2008-01-11 18:36:58 +0000

    another infrequent update of vulcan testware

---
+++ VulcanJ/ddl/vulcan/bad_idx.sql
@@ -0,0 +1,34 @@
+-- core-1040 from fb 2.0
+-- Wrong single-segment ascending index on character field with NULL and
+-- empty string values
+create database 'test.fdb';
+recreate table t (str varchar(10));
+commit;
+
+insert into t values ('');
+insert into t values (null);
+commit;
+
+create index t_i on t (str);
+commit;
+
+--result is 1, as expected
+select count(*) from t where str = '';
+
+--result is 0, wrong
+select count(*) from t where str is null;
+
+-- fixed by vlad, no tracker id that i found
+set sqlstate on;
+create table buggg (f1 int not null, f2 int not null);
+commit;
+
+insert into buggg values (1, 1);
+commit;
+
+alter table buggg add pk int not null primary key;
+-- alter table buggg add constraint pk_buggg primary key (pk);
+-- alter table buggg add constraint pk_buggg primary key (f1, f2, pk);
+
+drop database;
+quit;
--- VulcanJ/ddl/vulcan/long_id.sql
+++ VulcanJ/ddl/vulcan/long_id.sql
@@ -1,13 +1,28 @@
 set names ascii;
 -- s0363759 identifier is too long in fb2
 
-create database 'test.fdb'; 
+create database 'test.fdb';
+
+set sqlstate on;
+
+-- this create table statement fails, since table name > 31 characters
+create table test567890test567890test567890test567890 (i integer);
 
 -- this create table statement fails, since column name > 31 characters
 create table test1 (test567890test567890test567890test567890 integer);
 
 create table fred (i integer);
 insert into fred values (1);
+
+-- this select will fail, since column is unknown.
+select test567890test567890test567890test567890 from fred;
+
+-- but a long alias is ok? Seems fishy
 select i as test567890test567890test567890test567890 from fred;
+
+-- wbo: TODO this should say column name too long, but gives old message.
+-- wbo: that's because the new column is a string, not a MetaName
+-- "Name longer than database column size"
+alter table fred alter column i to test567890test567890test567890test567890;
 
 drop database;
