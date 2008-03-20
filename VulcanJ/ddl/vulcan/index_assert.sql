9066293b3eecfd4f3ebcd4a8118f866bd4562c11
Bill Oliver <verbguy@users.sourceforge.net>
2008-03-20 20:54:00 +0000

    occasional sync of vulcan test ware

--- VulcanJ/ddl/vulcan/index_assert.sql
+++ VulcanJ/ddl/vulcan/index_assert.sql
@@ -1,6 +1,8 @@
 create database 'test.fdb' user 'sysdba' password 'masterkey' 
    page_size=16834 default character set iso8859_1; 
 
+set sqlstate on;
+
 recreate table t (i integer not null primary key,
    c char(10) character set unicode_fss); 
 
